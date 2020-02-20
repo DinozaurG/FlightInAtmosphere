bb23990624e573b169cff4c0de1f4e32a5660edf
Dinozaur_G <karnah@mail.ru>
2020-02-20 16:09:08 +0700

    Что бы добавить?

--- FlightInAtmosphere/Form1.cs
+++ FlightInAtmosphere/Form1.cs
@@ -12,16 +12,13 @@ namespace FlightInAtmosphere
 {
     public partial class Form1 : Form
     {
-        decimal t = 0, x = 0, y, v0, cosA, sinA, S, m, k, vX, vY;
-
-
-
+        decimal t = 0, cosA, sinA, k, vX, vY;
+        Object ball = new Object();
         const decimal dt = 0.01M, g = 9.81M, C = 0.15M, rho = 1.29M;
         public Form1()
         {
             InitializeComponent();
         }
-
         private void button1_Click(object sender, EventArgs e)
         {
             if (inputWeight.Value != 0)
@@ -29,22 +26,22 @@ namespace FlightInAtmosphere
                 if (t == 0)
                 {
                     chart1.Series[0].Points.Clear(); //А оно ТУТ надо вообще?
-                    x = 0;
-                    y = inputHeight.Value;
-                    v0 = inputSpeed.Value;
+                    ball.x = 0;
+                    ball.y = inputHeight.Value;
+                    ball.v0 = inputSpeed.Value;
                     double alpha = (double)inputAngle.Value * Math.PI / 180;
                     cosA = (decimal)Math.Cos(alpha);
                     sinA = (decimal)Math.Sin(alpha);
-                    S = inputSize.Value;
-                    m = inputWeight.Value;
-                    k = 0.5M * C * rho * S / m;
-                    vX = v0 * cosA;
-                    vY = v0 * sinA;
+                    ball.S = inputSize.Value;
+                    ball.m = inputWeight.Value;
+                    k = 0.5M * C * rho * ball.S / ball.m;
+                    vX = ball.v0 * cosA;
+                    vY = ball.v0 * sinA;
                     chart1.ChartAreas[0].AxisX.Minimum = 0;
                     chart1.ChartAreas[0].AxisY.Maximum = 0;
-                    chart1.ChartAreas[0].AxisX.Maximum = (double)(v0 * v0 * (decimal)Math.Sin(2 * alpha) / g); //А есть формулы для этого в данном случае? А то я не нашёл
-                    chart1.ChartAreas[0].AxisY.Maximum = (double)(v0 * v0 * sinA * sinA / (2 * g));
-                    chart1.Series[0].Points.AddXY(x, y);
+                    chart1.ChartAreas[0].AxisX.Maximum = (double)(ball.v0 * ball.v0 * (decimal)Math.Sin(2 * alpha) / g); //А есть формулы для этого в данном случае? А то я не нашёл
+                    chart1.ChartAreas[0].AxisY.Maximum = (double)(ball.v0 * ball.v0 * sinA * sinA / (2 * g));
+                    chart1.Series[0].Points.AddXY(ball.x, ball.y);
                     timer.Start();
                 }
                 else
@@ -62,11 +59,11 @@ namespace FlightInAtmosphere
             decimal v = (decimal)Math.Sqrt((double)(vX * vX + vY * vY));
             vX -= (k * vX * v * dt);
             vY -= ((g + k * vY * v) * dt);
-            x += vX * dt;
-            y += vY * dt;
-            chart1.Series[0].Points.AddXY(x, y);
+            ball.x += vX * dt;
+            ball.y += vY * dt;
+            chart1.Series[0].Points.AddXY(ball.x, ball.y);
             timeShow.Text = ("Seconds: " + t);
-            if (y <= 0)
+            if (ball.y <= 0)
             {
                 t = 0;
                 timer.Stop();
---
+++ FlightInAtmosphere/Object.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FlightInAtmosphere
+{
+    class Object
+    {
+        public decimal t = 0, x = 0, y, v0, cosA, sinA, S, m, k, vX, vY;
+    }
+}
